c92d137cbfa49d42d11f1d4633cf458ec83690bb
John Smith <example@stanford.edu>
2013-07-07 22:14:39 -0400

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  fs.readfile('index.html', function (err, data) {
+    if (err) throw err;
+    response.send(buf.toString(data));
+  });
+
 });
 
 var port = process.env.PORT || 5000;
