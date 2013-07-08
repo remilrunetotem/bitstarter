ae084da9c5686e9dc285bfa9dcbf38ed07bc93cd
John Smith <example@stanford.edu>
2013-07-07 22:19:15 -0400

    Fixed web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-  fs.readfile('index.html', function (err, data) {
+  fs.readFileSync('index.html', function (err, data) {
     if (err) throw err;
     response.send(buf.toString(data));
   });
