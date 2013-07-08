4c9abcb426f06795d86fd5f7b39012d8d8cb378f
John Smith <example@stanford.edu>
2013-07-07 22:30:17 -0400

    Fixed web.js again

--- web.js
+++ web.js
@@ -5,10 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-  fs.readFileSync('index.html', function (err, data) {
-    if (err) throw err;
-    response.send(data.toString());
-  });
+  var data = fs.readFileSync('index.html');
+  response.send(data.toString());
 
 });
 
